3679d07b8f1bb2c3b5ae6aabbbd8ce830877d779
Arseniy Terekhin <senyai@gmail.com>
2022-07-18 19:03:36 +0300

    unility.ts: add `isInArray` function

--- src/util.ts
+++ src/util.ts
@@ -99,3 +99,8 @@ export function partition<T>(
 export async function delay(millis: number): Promise<any> {
     return new Promise((c, _e) => setTimeout(c, millis));
 }
+
+export const isInArray = <T, A extends T>(
+    item: T,
+    array: ReadonlyArray<A>
+): item is A => array.includes(item as A);
