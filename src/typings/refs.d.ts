0d4170c4ef919efdf0b13f4a4bcbf570dad7c83a
Arseniy Terekhin <senyai@gmail.com>
2023-03-11 16:25:31 +0300

    devfeat: `.filter(Boolean)` to return NonFalsy[] type

--- src/typings/refs.d.ts
+++ src/typings/refs.d.ts
@@ -5,3 +5,17 @@
  *--------------------------------------------------------------------------------------------*/
 
 /// <reference types='@types/node'/>
+
+declare namespace TSReset {
+    type NonFalsy<T> = T extends false | 0 | '' | null | undefined | 0n
+        ? never
+        : T;
+}
+
+interface Array<T> {
+    filter(predicate: BooleanConstructor, thisArg?: any): TSReset.NonFalsy<T>[];
+}
+
+interface ReadonlyArray<T> {
+    filter(predicate: BooleanConstructor, thisArg?: any): TSReset.NonFalsy<T>[];
+}
