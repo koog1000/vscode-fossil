a6f89e761825c49f21f6b117f673a1ce7519c9a9
Arseniy Terekhin <senyai@gmail.com>
2023-03-17 11:32:19 +0300

    test: ignore error handling in tests for coverage

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -24,6 +24,7 @@ async function main() {
             extensionTestsPath,
             launchArgs: [testWorkspace, '--disable-extensions'],
         });
+        /* c8 ignore next 4 */
     } catch (err) {
         console.error('Failed to run tests');
         process.exit(1);
