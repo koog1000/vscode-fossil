8fe113c1805fc3bde34503f452f332ca6c598150
Arseniy Terekhin <senyai@gmail.com>
2023-03-28 22:25:49 +0300

    test: split coverage report into test / code

--- src/test/summary_as_markdown.ts
+++ src/test/summary_as_markdown.ts
@@ -8,6 +8,7 @@ interface Details {
     skipped: number;
     pct: number;
 }
+
 interface Info {
     lines: Details;
     statements: Details;
@@ -18,6 +19,45 @@ interface Info {
 
 interface Summary {
     [key: string]: Info;
+}
+
+function addDetails(a: Details, b: Details): Details {
+    const total = a.total + b.total;
+    const covered = a.covered + b.covered;
+
+    return {
+        total,
+        covered,
+        skipped: a.skipped + b.skipped,
+        pct: parseFloat(((covered / total) * 100).toFixed(2)),
+    };
+}
+
+function filterSummary(summary: Summary, predicate: (key: string) => boolean) {
+    const entries = Object.entries(summary);
+    const filtered = entries.filter(
+        ([key, _info]) => key !== 'total' && predicate(key)
+    );
+    return Object.fromEntries(filtered);
+}
+
+function calcTotal(summary: Summary): Info {
+    let branches: Details = { covered: 0, pct: 0, skipped: 0, total: 0 };
+    let lines: Details = { covered: 0, pct: 0, skipped: 0, total: 0 };
+    let statements: Details = { covered: 0, pct: 0, skipped: 0, total: 0 };
+    let functions: Details = { covered: 0, pct: 0, skipped: 0, total: 0 };
+    for (const info of Object.values(summary)) {
+        branches = addDetails(branches, info.branches);
+        lines = addDetails(lines, info.lines);
+        statements = addDetails(statements, info.statements);
+        functions = addDetails(functions, info.functions);
+    }
+    return {
+        branches,
+        lines,
+        statements,
+        functions,
+    };
 }
 
 function formatDetails(details: Details): string {
@@ -32,6 +72,28 @@ function formatDetails(details: Details): string {
     return `${mark} ${details.pct}% (${details.covered}/${details.total})`;
 }
 
+function writeSummary(summary: Summary): string {
+    const lines: string[] = [];
+    lines.push('|File|🙈 Lines|🙉 Branches|🙊 Functions|');
+    lines.push('|-|-|-|-|');
+    const total = calcTotal(filterSummary(summary, Boolean));
+    lines.push(
+        `|total|${formatDetails(total.lines)}|${formatDetails(
+            total.branches
+        )}|${formatDetails(total.functions)}|`
+    );
+    for (const [key, info] of Object.entries(summary)) {
+        const split = key.indexOf('/src/');
+        const normalized = split == -1 ? key : key.slice(split + 1);
+        lines.push(
+            `|${normalized}|${formatDetails(info.lines)}|${formatDetails(
+                info.branches
+            )}|${formatDetails(info.functions)}|`
+        );
+    }
+    return lines.join('\n');
+}
+
 async function main(
     srcPath = 'coverage/coverage-summary.json',
     dstPath?: string
@@ -43,18 +105,20 @@ async function main(
         return 1;
     }
     const lines: string[] = [];
-    lines.push('|File|🙈 Lines|🙉 Branches|🙊 Functions|');
-    lines.push('|-|-|-|-|');
-    for (const [key, info] of Object.entries(obj as Summary)) {
-        const split = key.indexOf('/src/');
-        const normalized = split == -1 ? key : key.slice(split + 1);
-        lines.push(
-            `|${normalized}|${formatDetails(info.lines)}|${formatDetails(
-                info.branches
-            )}|${formatDetails(info.functions)}|`
-        );
-    }
+    lines.push('### Code\n');
+    lines.push(
+        writeSummary(
+            filterSummary(obj as Summary, key => key.indexOf('src/test') == -1)
+        )
+    );
+    lines.push('\n### Test\n');
+    lines.push(
+        writeSummary(
+            filterSummary(obj as Summary, key => key.indexOf('src/test') != -1)
+        )
+    );
     lines.push('');
+
     const out = lines.join('\n');
     if (dstPath) {
         await fs.writeFile(dstPath, out);
