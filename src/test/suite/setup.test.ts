588f5470dc5662869b73b42a085af2034a82b7f7
Arseniy Terekhin <senyai@gmail.com>
2023-05-30 22:17:01 +0300

    test: add 'clone' suite

--- src/test/suite/setup.test.ts
+++ src/test/suite/setup.test.ts
@@ -4,6 +4,7 @@ import * as sinon from 'sinon';
 import * as assert from 'assert/strict';
 import { FossilCWD } from '../../fossilExecutable';
 import { afterEach } from 'mocha';
+import * as os from 'os';
 
 suite('Setup', () => {
     const sandbox = sinon.createSandbox();
@@ -34,4 +35,257 @@ suite('Setup', () => {
             );
         });
     }).timeout(5000);
+    suite('Clone', function () {
+        test('Empty URI', async () => {
+            const showInputBox = sandbox
+                .stub(vscode.window, 'showInputBox')
+                .resolves('');
+            await vscode.commands.executeCommand('fossil.clone');
+            sinon.assert.calledOnce(showInputBox);
+        });
+        test('URI without auth', async () => {
+            const showInputBox = sandbox.stub(vscode.window, 'showInputBox');
+            const inputURI = showInputBox
+                .withArgs(sinon.match({ prompt: 'Repository URI' }))
+                .resolves('https://example.com/fossil');
+            const inputUsername = showInputBox
+                .withArgs(sinon.match({ prompt: 'Username' }))
+                .resolves('testuser');
+            const inputPassword = showInputBox
+                .withArgs(sinon.match({ prompt: 'User Authentication' }))
+                .resolves('testpsw');
+            const showInformationMessage = (
+                sandbox.stub(
+                    vscode.window,
+                    'showInformationMessage'
+                ) as sinon.SinonStub
+            )
+                .withArgs(
+                    'Would you like to open the cloned repository?',
+                    'Open Repository'
+                )
+                .resolves();
+
+            const showSaveDialogstub = sandbox
+                .stub(vscode.window, 'showSaveDialog')
+                .withArgs(
+                    sinon.match({ title: 'Select New Fossil File Location' })
+                )
+                .resolves(
+                    vscode.Uri.joinPath(
+                        vscode.Uri.file(os.tmpdir()),
+                        'test_path'
+                    )
+                );
+
+            const executable = getExecutable();
+
+            const execStub = sandbox.stub(executable, 'exec');
+
+            await vscode.commands.executeCommand('fossil.clone');
+            sinon.assert.calledOnce(inputURI);
+            sinon.assert.calledOnce(inputUsername);
+            sinon.assert.calledOnce(inputPassword);
+            sinon.assert.calledOnce(showSaveDialogstub);
+            sinon.assert.calledOnce(showInformationMessage);
+            sinon.assert.calledOnceWithExactly(
+                execStub,
+                os.tmpdir() as FossilCWD,
+                [
+                    'clone',
+                    'https://testuser:testpsw@example.com/fossil',
+                    '/tmp/test_path',
+                    '--verbose',
+                ]
+            );
+        });
+        test('URI with username and password', async () => {
+            const showInputBox = sandbox.stub(vscode.window, 'showInputBox');
+            const inputURI = showInputBox
+                .withArgs(sinon.match({ prompt: 'Repository URI' }))
+                .resolves('https://testuser:testpsw@example.com/fossil');
+            const showInformationMessage = (
+                sandbox.stub(
+                    vscode.window,
+                    'showInformationMessage'
+                ) as sinon.SinonStub
+            )
+                .withArgs(
+                    'Would you like to open the cloned repository?',
+                    'Open Repository'
+                )
+                .resolves();
+
+            const showSaveDialogstub = sandbox
+                .stub(vscode.window, 'showSaveDialog')
+                .withArgs(
+                    sinon.match({ title: 'Select New Fossil File Location' })
+                )
+                .resolves(
+                    vscode.Uri.joinPath(
+                        vscode.Uri.file(os.tmpdir()),
+                        'test_path'
+                    )
+                );
+
+            const executable = getExecutable();
+
+            const execStub = sandbox.stub(executable, 'exec');
+
+            await vscode.commands.executeCommand('fossil.clone');
+            sinon.assert.calledOnce(inputURI);
+            sinon.assert.calledOnce(showSaveDialogstub);
+            sinon.assert.calledOnce(showInformationMessage);
+            sinon.assert.calledOnceWithExactly(
+                execStub,
+                os.tmpdir() as FossilCWD,
+                [
+                    'clone',
+                    'https://testuser:testpsw@example.com/fossil',
+                    '/tmp/test_path',
+                    '--verbose',
+                ]
+            );
+        });
+        test('URI with username only', async () => {
+            const showInputBox = sandbox.stub(vscode.window, 'showInputBox');
+            const inputURI = showInputBox
+                .withArgs(sinon.match({ prompt: 'Repository URI' }))
+                .resolves('https://testuser@example.com/fossil');
+            const inputPassword = showInputBox
+                .withArgs(sinon.match({ prompt: 'User Authentication' }))
+                .resolves('testpsw');
+            const showInformationMessage = (
+                sandbox.stub(
+                    vscode.window,
+                    'showInformationMessage'
+                ) as sinon.SinonStub
+            )
+                .withArgs(
+                    'Would you like to open the cloned repository?',
+                    'Open Repository'
+                )
+                .resolves();
+
+            const showSaveDialogstub = sandbox
+                .stub(vscode.window, 'showSaveDialog')
+                .withArgs(
+                    sinon.match({ title: 'Select New Fossil File Location' })
+                )
+                .resolves(
+                    vscode.Uri.joinPath(
+                        vscode.Uri.file(os.tmpdir()),
+                        'test_path'
+                    )
+                );
+
+            const executable = getExecutable();
+
+            const execStub = sandbox.stub(executable, 'exec');
+
+            await vscode.commands.executeCommand('fossil.clone');
+            sinon.assert.calledOnce(inputURI);
+            sinon.assert.calledOnce(inputPassword);
+            sinon.assert.calledOnce(showSaveDialogstub);
+            sinon.assert.calledOnce(showInformationMessage);
+            sinon.assert.calledOnceWithExactly(
+                execStub,
+                os.tmpdir() as FossilCWD,
+                [
+                    'clone',
+                    'https://testuser:testpsw@example.com/fossil',
+                    '/tmp/test_path',
+                    '--verbose',
+                ]
+            );
+        });
+        test('Cancel username', async () => {
+            const showInputBox = sandbox.stub(vscode.window, 'showInputBox');
+            const inputURI = showInputBox
+                .withArgs(sinon.match({ prompt: 'Repository URI' }))
+                .resolves('https://example.com/fossil');
+            const inputUsername = showInputBox
+                .withArgs(sinon.match({ prompt: 'Username' }))
+                .resolves();
+            await vscode.commands.executeCommand('fossil.clone');
+            sinon.assert.calledOnce(inputURI);
+            sinon.assert.calledOnce(inputUsername);
+        });
+        test('Empty password', async () => {
+            const showInputBox = sandbox.stub(vscode.window, 'showInputBox');
+            const inputURI = showInputBox
+                .withArgs(sinon.match({ prompt: 'Repository URI' }))
+                .resolves('https://example.com/fossil');
+            const inputUsername = showInputBox
+                .withArgs(sinon.match({ prompt: 'Username' }))
+                .resolves('testuser');
+            const inputPassword = showInputBox
+                .withArgs(sinon.match({ prompt: 'User Authentication' }))
+                .resolves('');
+            const showInformationMessage = (
+                sandbox.stub(
+                    vscode.window,
+                    'showInformationMessage'
+                ) as sinon.SinonStub
+            )
+                .withArgs(
+                    'Would you like to open the cloned repository?',
+                    'Open Repository'
+                )
+                .resolves();
+
+            const showSaveDialogstub = sandbox
+                .stub(vscode.window, 'showSaveDialog')
+                .withArgs(
+                    sinon.match({ title: 'Select New Fossil File Location' })
+                )
+                .resolves(
+                    vscode.Uri.joinPath(
+                        vscode.Uri.file(os.tmpdir()),
+                        'test_path'
+                    )
+                );
+
+            const executable = getExecutable();
+
+            const execStub = sandbox.stub(executable, 'exec');
+
+            await vscode.commands.executeCommand('fossil.clone');
+            sinon.assert.calledOnce(inputURI);
+            sinon.assert.calledOnce(inputUsername);
+            sinon.assert.calledOnce(inputPassword);
+            sinon.assert.calledOnce(showSaveDialogstub);
+            sinon.assert.calledOnce(showInformationMessage);
+            sinon.assert.calledOnceWithExactly(
+                execStub,
+                os.tmpdir() as FossilCWD,
+                [
+                    'clone',
+                    'https://testuser@example.com/fossil',
+                    '/tmp/test_path',
+                    '--verbose',
+                ]
+            );
+        });
+        test('Cancel on path', async () => {
+            const showInputBox = sandbox.stub(vscode.window, 'showInputBox');
+            const inputURI = showInputBox
+                .withArgs(sinon.match({ prompt: 'Repository URI' }))
+                .resolves('https://testuser:testpsw@example.com/fossil');
+            const showSaveDialogstub = sandbox
+                .stub(vscode.window, 'showSaveDialog')
+                .withArgs(
+                    sinon.match({ title: 'Select New Fossil File Location' })
+                )
+                .resolves();
+
+            const executable = getExecutable();
+            const execStub = sandbox.spy(executable, 'exec');
+
+            await vscode.commands.executeCommand('fossil.clone');
+            sinon.assert.calledOnce(inputURI);
+            sinon.assert.calledOnce(showSaveDialogstub);
+            sinon.assert.notCalled(execStub);
+        });
+    });
 });
