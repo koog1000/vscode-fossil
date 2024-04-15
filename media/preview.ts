004950679757c2d37a8cddfe43ea792dbdf6a4e7
Arseniy Terekhin <senyai@gmail.com>
2024-04-15 11:50:45 +0300

    fix: some rendered text elements were missing
    basically, pikchr `print` statement output was ignored (by node type and by color)

--- media/preview.ts
+++ media/preview.ts
@@ -1,23 +1,27 @@
 const $ = (s: string) => document.getElementById(s);
 const vscode = acquireVsCodeApi<{ uri: string }>();
+type Nodes = NodeListOf<ChildNode>;
 
-function htmlToElements(html: string): HTMLCollection {
+function htmlToElements(html: string): Nodes {
     const template = document.createElement('template');
     template.innerHTML = html;
-    return template.content.children;
+    return template.content.childNodes;
 }
 
-function find_diff(olds: HTMLCollection, news: HTMLCollection): Element {
+function find_diff(olds: Nodes, news: Nodes): ChildNode {
     const n = Math.max(olds.length, news.length);
     for (let i = 0; i < n; ++i) {
         if (olds[i] && news[i]) {
-            if (olds[i].isEqualNode(news[i])) {
+            if (
+                olds[i].isEqualNode(news[i]) ||
+                news[i].nodeType == Node.COMMENT_NODE
+            ) {
                 continue;
             } else {
-                if (!news[i].children.length) {
+                if (!news[i].childNodes.length) {
                     return news[i];
                 }
-                return find_diff(olds[i].children, news[i].children);
+                return find_diff(olds[i].childNodes, news[i].childNodes);
             }
         } else {
             return news[i];
@@ -26,13 +30,17 @@ function find_diff(olds: HTMLCollection, news: HTMLCollection): Element {
     return news[0];
 }
 
-function scrollIntoViewIfNeeded(target: Element): void {
-    if (target.getBoundingClientRect().bottom > window.innerHeight) {
-        target.scrollIntoView(false);
+function scrollIntoViewIfNeeded(target: ChildNode): void {
+    while (target.nodeType != target.ELEMENT_NODE) {
+        target = target.previousSibling ?? target.parentElement;
     }
-
-    if (target.getBoundingClientRect().top < 0) {
-        target.scrollIntoView();
+    const rect = (target as Element).getBoundingClientRect();
+    if (rect.bottom > window.innerHeight || rect.top < 0) {
+        (target as Element).scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center',
+        });
     }
 }
 
@@ -41,7 +49,7 @@ window.addEventListener(
     (event: MessageEvent<{ html: string; uri: string }>): void => {
         const news = htmlToElements(event.data.html);
         const content = $('fossil-preview-content');
-        const diff_el = find_diff(content.children, news);
+        const diff_el = find_diff(content.childNodes, news);
         content.replaceChildren(...news);
         vscode.setState({ uri: event.data.uri });
         if (diff_el) {
