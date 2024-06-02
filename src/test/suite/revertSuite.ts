d0480c4be7d2ecbba2822c94e8f32f2e62bbe0ce
Arseniy Terekhin <senyai@gmail.com>
2024-06-02 20:38:49 +0300

    test: more revert dialog text tests

--- src/test/suite/revertSuite.ts
+++ src/test/suite/revertSuite.ts
@@ -3,8 +3,9 @@ import * as sinon from 'sinon';
 import { add, fakeFossilStatus, getExecStub, getRepository } from './common';
 import * as assert from 'assert/strict';
 import * as fs from 'fs/promises';
-import { Suite } from 'mocha';
-import { FossilResourceGroup } from '../../resourceGroups';
+import type { Suite } from 'mocha';
+import type { FossilResourceGroup } from '../../resourceGroups';
+import type { FossilResource } from '../../repository';
 
 export function RevertSuite(this: Suite): void {
     test('Single source', async () => {
@@ -31,50 +32,101 @@ export function RevertSuite(this: Suite): void {
         assert.equal(newContext.toString('utf-8'), 'Some original text\n');
     });
 
-    test('Dialog has no typos', async () => {
-        const repository = getRepository();
-        const rootUri = workspace.workspaceFolders![0].uri;
-        const fake_status = [];
-        const execStub = getExecStub(this.ctx.sandbox);
-        const fileUris: Uri[] = [];
-        for (const filename of 'abcdefghijklmn') {
-            const fileUri = Uri.joinPath(rootUri, 'added', filename);
-            fake_status.push(`EDITED     added/${filename}`);
-            fileUris.push(fileUri);
-        }
-        const statusCall = fakeFossilStatus(execStub, fake_status.join('\n'));
-        await repository.updateModelState();
-        sinon.assert.calledOnce(statusCall);
-        const resources = fileUris.map(uri => {
-            const resource = repository.workingGroup.getResource(uri);
-            assert.ok(resource);
-            return resource;
+    suite('Dialog has no typos', () => {
+        let swmResources: FossilResource[];
+        const prepareResources = async () => {
+            if (swmResources) {
+                return swmResources;
+            }
+            const repository = getRepository();
+            const rootUri = workspace.workspaceFolders![0].uri;
+            const fake_status = [];
+            const execStub = getExecStub(this.ctx.sandbox);
+            const fileUris: Uri[] = [];
+            for (const filename of 'abcdefghijklmn') {
+                // 14 files
+                const fileUri = Uri.joinPath(rootUri, 'added', filename);
+                const action = ['k', 'l', 'm', 'n'].includes(filename)
+                    ? 'ADDED'
+                    : 'EDITED';
+                fake_status.push(`${action}     added/${filename}`);
+                fileUris.push(fileUri);
+            }
+            const statusCall = fakeFossilStatus(
+                execStub,
+                fake_status.join('\n')
+            );
+            await repository.updateModelState();
+            sinon.assert.calledOnce(statusCall);
+            const resources = fileUris.map(uri => {
+                const resource = repository.workingGroup.getResource(uri);
+                assert.ok(resource);
+                return resource;
+            });
+            swmResources = resources;
+            return swmResources;
+        };
+
+        test('10 + 4 files', async () => {
+            const resources = await prepareResources();
+            const swm = this.ctx.sandbox.stub(window, 'showWarningMessage');
+            await commands.executeCommand('fossil.revert', ...resources);
+            sinon.assert.calledOnceWithMatch(
+                swm,
+                'Are you sure you want to discard changes to 10 files?\n' +
+                    '\n • a\n • b\n • c\n • d\n • e\n • f\n • g\n • h\n' +
+                    'and 2 others\n\n(and forget 4 other added files)',
+                { modal: true }
+            );
         });
-        const swm: sinon.SinonStub = this.ctx.sandbox.stub(
-            window,
-            'showWarningMessage'
-        );
-        await commands.executeCommand('fossil.revert', ...resources);
-        assert.ok(
-            swm.firstCall.calledWith(
-                'Are you sure you want to discard changes to 14 files?\n\n • a\n • b\n • c\n • d\n • e\n • f\n • g\n • h\nand 6 others'
-            )
-        );
-        await commands.executeCommand(
-            'fossil.revert',
-            ...resources.slice(0, 3)
-        );
-        assert.ok(
-            swm.secondCall.calledWith(
-                'Are you sure you want to discard changes to 3 files?\n\n • a\n • b\n • c'
-            )
-        );
+
+        test('10  files', async () => {
+            const resources = await prepareResources();
+            const swm = this.ctx.sandbox.stub(window, 'showWarningMessage');
+            await commands.executeCommand(
+                'fossil.revert',
+                ...resources.slice(0, 10)
+            );
+            sinon.assert.calledOnceWithMatch(
+                swm,
+                'Are you sure you want to discard changes to 10 files?\n' +
+                    '\n • a\n • b\n • c\n • d\n • e\n • f\n • g\n • h\n' +
+                    'and 2 others',
+                { modal: true }
+            );
+        });
+
+        test('3 files', async () => {
+            const resources = await prepareResources();
+            const swm = this.ctx.sandbox.stub(window, 'showWarningMessage');
+            await commands.executeCommand(
+                'fossil.revert',
+                ...resources.slice(0, 3)
+            );
+            sinon.assert.calledOnceWithMatch(
+                swm,
+                'Are you sure you want to discard changes to 3 files?\n' +
+                    '\n • a\n • b\n • c',
+                { modal: true }
+            );
+        });
+
+        test('2 added file', async () => {
+            const resources = await prepareResources();
+            const swm = this.ctx.sandbox.stub(window, 'showWarningMessage');
+            await commands.executeCommand(
+                'fossil.revert',
+                ...resources.slice(10, 12)
+            );
+            sinon.assert.notCalled(swm);
+        });
     });
 
     test('Revert (Nothing)', async () => {
         await commands.executeCommand('fossil.revert');
     });
 
+    // for testing `fossil.revertAll` only
     async function revertAllTest(
         sandbox: sinon.SinonSandbox,
         groups: FossilResourceGroup[],
